c40917bc9e73fbd3cb10c18f5b982a8fbc43f774
Matrix Marketers <matrixm@sanjays-Mac.local>
2022-07-12 17:23:30 +0530

    initial function

---
+++ adopshun-ios/AdopshunCreator.swift
@@ -0,0 +1,13 @@
+//
+//  AdopshunCreator.swift
+//  adopshun-ios
+//
+//  Created by Matrix Marketers on 12/07/22.
+//
+
+import Foundation
+public struct AdopshunCreator{
+    static func sayHello(){
+        print("------------>>>>>  Hello adopshun  <<<<<<<<------------")
+    }
+}
